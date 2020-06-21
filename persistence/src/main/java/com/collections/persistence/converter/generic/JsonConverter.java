635ef1461bc602a9f5fa274972a387c5bb1717d7
Lukasz <kuchtalukasz89@gmail.com>
2020-06-22 00:18:03 +0200

    Added generic JSON Converter

---
+++ persistence/src/main/java/com/collections/persistence/converter/generic/JsonConverter.java
@@ -0,0 +1,44 @@
+package com.collections.persistence.converter.generic;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.collections.persistence.exception.PersistenceException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Optional;
+
+public abstract class JsonConverter<T> {
+
+    private final String jsonFilename;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private final Type type = ((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+
+    public JsonConverter(String jsonFilename) {
+        this.jsonFilename = jsonFilename;
+    }
+
+    // conversion from object to json
+    public void toJson(final T element) {
+        try (FileWriter fileWriter = new FileWriter(jsonFilename)) {
+            if (element == null) {
+                throw new NullPointerException("ELEMENT IS NULL");
+            }
+            gson.toJson(element, fileWriter);
+        } catch (Exception e) {
+            throw new PersistenceException("TO JSON - JSON FILENAME EXCEPTION");
+        }
+    }
+
+    // conversion from json to object
+    public Optional<T> fromJson() {
+        try (FileReader fileReader = new FileReader(jsonFilename)) {
+            return Optional.of(gson.fromJson(fileReader, type));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new PersistenceException("FROM JSON - JSON FILENAME EXCEPTION");
+        }
+    }
+}
