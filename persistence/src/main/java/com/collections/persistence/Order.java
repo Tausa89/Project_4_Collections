21a353c9794f55281a005840ac50abe4f7358fbe
Lukasz <kuchtalukasz89@gmail.com>
2020-06-22 00:07:15 +0200

    Added exemplary JSON file and helper class which one is connecting Customer and Product class

---
+++ persistence/src/main/java/com/collections/persistence/Order.java
@@ -0,0 +1,19 @@
+package com.collections.persistence;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    private Customer customer;
+    private List<Product> products;
+}
